6fc8853d5157dd2b1a8f0447b88690b77b6406e6
jon <jonathanyusf@gmail.com>
2021-06-01 15:26:03 -0700

    Add users table with complete user model

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+  validates :username, :email, :session_token, presence :true, uniqueness :true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  before_validation :ensure_session_token
+  attr_reader :password
+  #SPIRE
+
+  def self.find_by_credentials(username, password)
+      @user = User.find_by(username: username)
+      if @user && @user.is_password?(password)
+        return @user
+      else
+        return nil
+      end
+  end
+
+  def password=(password)
+    @password = password 
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    pw_ob = BCrypt::Password.new(self.password_digest)
+    pw_ob.is_password?(password)
+  end 
+
+  def reset_session_token!
+    self.session_token = SecureRandom.base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token 
+    self.session_token ||= SecureRandom.base64
+  end
+  
+end
