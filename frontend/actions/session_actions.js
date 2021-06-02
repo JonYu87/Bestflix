175100f06a7ba5a3a6e66b151c644bde49a438ef
jon <jonathanyusf@gmail.com>
2021-06-02 16:33:27 -0700

    Add users_reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -30,6 +30,6 @@ export const login = user => dispatch (
 ); 
 
 export const logout = () => dispatch => (
-  APIUtil.logout().then(user => (dispatch(logoutCurrentUser())))
+  APIUtil.logout().then(() => (dispatch(logoutCurrentUser())))
 );
 
---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+const usersReducer = (state ={}, action) => {
+  Object.freeze(state);
+  let nextState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      nextState[action.currentUser] = action.currentUser
+      return nextState;
+    default:
+      return state;
+  }
+}
