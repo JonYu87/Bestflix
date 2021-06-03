1d9493cbd3c055822149d2d86415ba2973f5bf17
jon <jonathanyusf@gmail.com>
2021-06-03 11:38:08 -0700

    Working on login container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import NavBar from './navbar/nav_bar'
+
+
 
 const App = () => (
   <div>
     <h1>Bestflix!</h1>
+    <NavBar />
   </div>
 );
 
--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class navBar extends React.Component {
+class NavBar extends React.Component {
   constructor(props) {
     super(props)
   }
 
   render () {
+    return (
     <div>
       Placeholder for NavBar
+      <br />
+      <Link to="/login">Login</Link>
+      <br />
+      <Link to="/signup">Sign up!</Link>
+      <br />
+      <button onClick={this.props.logout}>Log Out</button>
+      <br />
     </div>
+    )
   }
 };
 
-export default navBar;
+export default NavBar;
