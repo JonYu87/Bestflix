6d6aff88a4dd3ef6cd89b02b2af639d283c59874
jon <jonathanyusf@gmail.com>
2021-06-03 16:32:30 -0700

    Add bootstrap user functionality

--- frontend/bestflix.jsx
+++ frontend/bestflix.jsx
@@ -6,7 +6,20 @@ import * as APIUtil from './util/session_api_util'
 
 
 document.addEventListener("DOMContentLoaded",() => {
-  const store = configureStore();
+  let store;
+  
+if (window.currentUser) {
+  const preloadedState = {
+    entities: {
+      users: { [window.currentUser.id]: window.currentUser }
+    },
+    session: { id: window.currentUser.id }
+  };
+  store = configureStore(preloadedState);
+  delete window.currentUser;
+} else {
+  store = configureStore();
+}
 
   //For testing purposes
   window.login = APIUtil.login
