c7a1a660a3f95e5c0374bc17cd6e76509be3c9d9
jon <jonathanyusf@gmail.com>
2021-06-10 22:10:54 -0700

    Add show description on hover

--- frontend/components/movies/movies_index_item.jsx
+++ frontend/components/movies/movies_index_item.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import MovieHover from './movie_hover';
 import MovieShow from './movie_show';
 
 class MoviesIndexItem extends React.Component {
@@ -8,18 +9,37 @@ class MoviesIndexItem extends React.Component {
 
     this.state = {
       poster: null,
-      video: null
+      video: null,
+      isHovering: false
     }
+    this.handleMouseEnter = this.handleMouseEnter.bind(this)
+  }
+
+
+  handleMouseEnter (e) {
+    e.preventDefault();
+    this.setState({isHovering: true})
+    console.log(this.props)
+  }
+
+  handleMouseOut (e) {
+    e.preventDefault();
+    this.setState({isHovering: false})
   }
 
   render () {
+    const {movie} =this.props
     return (
       <div className="wrapping-div">
         <div className="carousel-container">
-          <Link to={`/api/movies/${this.props.movie.id}`} className="item">
-            <img src={this.props.movie.poster}/>
+          <Link to={`/api/movies/${movie.id}`} className="item">
+            <img src={movie.poster} 
+            onMouseEnter={e => this.handleMouseEnter(e)}
+            onMouseOut={e => this.handleMouseOut(e)}
+            />
           </Link>
         </div>
+          <MovieHover movie={movie} isHovering={this.state.isHovering}/>
       </div>
     )
   }
--- frontend/components/splash/splash_component.jsx
+++ frontend/components/splash/splash_component.jsx
@@ -7,11 +7,19 @@ class SplashComponent extends React.Component {
 
   render () {
     return (
+      <div>
+        <div className="splash-header">
+          <h1>Unlimited movies, TV shows, and more.</h1>
+          <h2>Watch anywhere. Cancel anytime.</h2>
+
+        </div>
+
         <div className="splash-footer">
           <div className="footer-wrapper">Clone By Jonathan Yu:
           <a className="footer-button" href="https://github.com/JonYu87/" target="_blank"><i className="fab fa-github footer-link"></i></a>
           </div>
         </div>
+      </div>
     )
   }
 };
