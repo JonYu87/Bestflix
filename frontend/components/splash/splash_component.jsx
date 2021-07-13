0d6fee79cb6b0548b1049f87c0cb27edc5448e93
jon <jonathanyusf@gmail.com>
2021-07-13 12:17:14 -0700

    Add tv to splash page

--- frontend/components/splash/splash_component.jsx
+++ frontend/components/splash/splash_component.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 class SplashComponent extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
-  render () {
+  render() {
     return (
       <div>
         <div className="splash-header">
@@ -14,26 +14,41 @@ class SplashComponent extends React.Component {
           <br />
           <h2>Watch anywhere. Cancel anytime.</h2>
           <br />
-          <Link to='/signup'>
+          <Link to="/signup">
             <button className="start-button">Get Started </button>
           </Link>
         </div>
 
         <div className="splash-body">
+          <div className="splash-body-text">
             <h1>Enjoy on your TV</h1>
-            <br />
-            <h2>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.
-            </h2>
+            <h3>
+              Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV,
+              Blu-ray players, and more.
+            </h3>
+          </div>
+
+          <img
+            className="splash-tv"
+            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+          />
         </div>
 
         <div className="splash-footer">
-          <div className="footer-wrapper">Clone By Jonathan Yu:
-          <a className="footer-button" href="https://github.com/JonYu87/" target="_blank"><i className="fab fa-github footer-link"></i></a>
+          <div className="footer-wrapper">
+            Clone By Jonathan Yu:
+            <a
+              className="footer-button"
+              href="https://github.com/JonYu87/"
+              target="_blank"
+            >
+              <i className="fab fa-github footer-link"></i>
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default SplashComponent;
