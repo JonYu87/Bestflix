ab73ad9ac453ed84ae7676d79104374b93cb83ef
jon <jonathanyusf@gmail.com>
2021-07-19 14:43:41 -0700

    Work on positioning of arrows for slider

--- frontend/components/movies/movies_index.jsx
+++ frontend/components/movies/movies_index.jsx
@@ -1,48 +1,50 @@
-import React from 'react'
-import MoviesIndexItem from './movies_index_item';
-import { Link } from 'react-router-dom'
+import React from "react";
+import MoviesIndexItem from "./movies_index_item";
+import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.scss";
-import "swiper/components/pagination/pagination.scss"
-import "swiper/components/navigation/navigation.scss"
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/navigation/navigation.scss";
 // import 'swiper/swiper-bundle.css';
 
-import SwiperCore, {
-  Pagination,Navigation
-} from 'swiper/core';
+import SwiperCore, { Pagination, Navigation } from "swiper/core";
 
-SwiperCore.use([Pagination,Navigation]);
+SwiperCore.use([Pagination, Navigation]);
 
 class MoviesIndex extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   componentDidMount() {
-    this.props.fetchMovies()
-  };
+    this.props.fetchMovies();
+  }
 
-  render () {
+  render() {
     return (
-
       <div className="browse-container">
         <h1 className="browse-header">Browse</h1>
-            <div className="row-wrapper">
-                <Swiper slidesPerView={5} spaceBetween={30} slidesPerGroup={1} loop={true} pagination={{
-                  "clickable": true
-                }} navigation={true} className="mySwiper">
-                {this.props.movies.map(movie => {
-                  return (
-                    <SwiperSlide>
-                    <MoviesIndexItem movie={movie} key={movie.id}/>
-                    </SwiperSlide>
-                    ) 
-                  })}
-                  </Swiper>
-            </div>
+        <Swiper
+          slidesPerView={5}
+          spaceBetween={30}
+          slidesPerGroup={3}
+          loop={true}
+          navigation={true}
+          className="mySwiper"
+        >
+          <div className="row-wrapper">
+            {this.props.movies.map((movie) => {
+              return (
+                <SwiperSlide>
+                  <MoviesIndexItem movie={movie} key={movie.id} />
+                </SwiperSlide>
+              );
+            })}
+          </div>
+        </Swiper>
       </div>
-    )
+    );
   }
-};
+}
 
 export default MoviesIndex;
