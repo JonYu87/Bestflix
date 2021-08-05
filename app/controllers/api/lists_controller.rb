7cccd1bcecf7b2e1f70ebea5b8af5fbd609c787d
jon <jonathanyusf@gmail.com>
2021-08-05 13:32:55 -0700

    Finish lists controller

---
+++ app/controllers/api/lists_controller.rb
@@ -0,0 +1,27 @@
+class Api::ListsController < ApplicationController
+  before_action :require_login, only: [:index :create, :destroy]
+
+  def index
+    @lists = current_user.lists
+    @lists ? render index : render json: { error: 'No list' }, status: 422
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @list ? render show: : render json: { error: 'No list' }, status: 422
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save ? render show : render json: @list.errors.full_messages, status: 422
+  end
+
+  def destroy
+    @list = List.find_by(movie_id: list_params[:movie_id], user_id: list_params[:user_id])
+    List.destroy(@list.id) ? render show, status: 200 : render json: @list.errors.full_messages, status: 422
+  end
+
+  def list_params
+    params.require(:list).permit(:movie_id, :user_id)
+  end
+end
