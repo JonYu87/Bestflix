a87006992329681ea50c86f6a5f17e52392aa5f0
jon <jonathanyusf@gmail.com>
2021-08-11 16:06:52 -0700

    Fix list bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_032039) do
+ActiveRecord::Schema.define(version: 2021_08_11_221102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2021_08_05_032039) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_lists_on_movie_id", unique: true
-    t.index ["user_id"], name: "index_lists_on_user_id", unique: true
   end
 
   create_table "movies", force: :cascade do |t|
