b41763b88660b78ad6ca92bed128239837ad5509
jon <jonathanyusf@gmail.com>
2022-03-17 13:03:27 -0700

    Refactor MovieShow to functional component with useEffect hook

--- frontend/components/movies/movie_show.jsx
+++ frontend/components/movies/movie_show.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
+const MovieShow = (props) => {
+  useEffect(() => {
+    this.props.fetchMovie(this.props.match.params.movieId);
+  }, []);
 
-class MovieShow extends React.Component {
-
-  componentDidMount() {
-    this.props.fetchMovie(this.props.match.params.movieId)
-  }
-  
-  render () {
-    if (!this.props.movie) {
-      return null;
-    } else {
-
-      return (
-        <div>
-          <div className="video-show-container">
-            <video src={this.props.movie.video} 
-            controls autoPlay
-            width="100%" height="50%"
-            >
-            </video>
-          </div>
+  if (!props.movie) {
+    return null;
+  } else {
+    return (
+      <div>
+        <div className="video-show-container">
+          <video
+            src={props.movie.video}
+            controls
+            autoPlay
+            width="100%"
+            height="50%"
+          ></video>
+        </div>
       </div>
-    )
+    );
   }
-}
 };
 
 export default MovieShow;
